8c198e576ddae36d420b5af02a82393c98fdaae1
danperes97 <danilo.peres@hotmail.com>
2018-05-26 15:22:03 -0300

    Refactor driver hash generator

--- lib/race_analyzer.ex
+++ lib/race_analyzer.ex
@@ -1,13 +1,13 @@
 defmodule RaceAnalyzer do
   def build_race_hash(log) do
     LogReader.read(log)
-    |> Enum.map(&(racer_hash(&1)))
+    |> Enum.map(&(driver_hash(&1)))
   end
 
-  def racer_hash(line) do
+  def driver_hash(line) do
     %{
       id:        Enum.at(line, 2),
-      racer:     Enum.at(line, 1),
+      driver:     Enum.at(line, 1),
       lap_time:  Enum.at(line, 3),
       time:      Enum.at(line, 0),
       average:   Enum.at(line, 4)
